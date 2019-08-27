8b9d0d9abdc329d99263ff126cd583695a2b3c15
Rafał Rzepecki <divided.mind@gmail.com>
2019-08-27 22:56:43 +0200

    Style: Add frozen string literal magic comment

--- lib/memtar.rb
+++ lib/memtar.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 require 'archive/tar/minitar'
 require 'memtar/version'
 
--- lib/memtar/version.rb
+++ lib/memtar/version.rb
@@ -1,4 +1,5 @@
-#
+# frozen_string_literal: true
+
 class MemTar
   VERSION = "0.1.0"
 end
